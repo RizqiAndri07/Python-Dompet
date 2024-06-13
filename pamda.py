c505c77f6dd3ac6e1d5d07c1a3279224f811a70b
Kinggu <rizqiandri12la@gmail.com>
2024-06-13 11:55:50 +0700

    Changes to be committed:  Changes not staged for commit:

--- pamda.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from tabulate import tabulate
-
-def mutasi():
-    df = pd.read_csv('mutasi.csv')
-    table = tabulate(df, headers='keys', tablefmt='pretty')
-    print(table)
-
-mutasi()
